3cda9ec8743ffa4f6f6111cfa16d60f50bf23d89
kitya <terekhovaekat@gmail.com>
2022-11-22 13:08:59 +0300

    Change wait_element_visibility functions

--- utils/wait_visibilty.py
+++ utils/wait_visibilty.py
@@ -8,20 +8,26 @@ class Waiter:
         self._logger = logger
         self._driver = driver
 
-    def _try_wait_for(self, wait_function, by, value) -> Callable:
+    def wait_until(self, condition, waiting_sec: int, msg: str):
         try:
-            wait_function(self, by, value)
+            WebDriverWait(self._driver, waiting_sec).until(condition)
+            return True
         except WebDriverException as ex:
             if not ex.msg.isspace():
-                self._logger.info(ex.msg)
-            self._logger.info(f"Element(s) wasn't loaded {value} by {by} in page: \n {self._driver.title}")
-        return False
+                self._logger.warning(ex.msg)
+            if msg:
+                self._logger.warning(msg)
+            return False
 
-    def wait_element_visibility(self, by, value, waiting_sec: int = 60):
-        WebDriverWait(self._driver, waiting_sec).until(ec.visibility_of_element_located((by, value)))
+    def wait_element_visibility(self, by, value, waiting_sec: int = 30):
+        msg = f"Element wasn't loaded {value} by {by} in page: \n {self._driver.title}"
+        condition = ec.visibility_of_element_located((by, value))
+        return self.wait_until(condition, waiting_sec, msg)
 
-    def wait_elements_visibility(self, by, value, waiting_sec: int = 60):
-        WebDriverWait(self._driver, waiting_sec).until(ec.visibility_of_all_elements_located((by, value)))
+    def wait_elements_visibility(self, by, value, waiting_sec: int = 30):
+        msg = f"Elements wasn't loaded {value} by {by} in page: \n {self._driver.title}"
+        condition = ec.visibility_of_all_elements_located((by, value))
+        return self.wait_until(condition, waiting_sec, msg)
 
 
 
