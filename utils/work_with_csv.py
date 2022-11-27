e86843033c070247befc539c2a23d1dc22f5ccba
Kitya <kittitereh@kittitereh.(none)>
2022-11-28 00:52:26 +0300

    add list unpacking before recording into csv

--- utils/work_with_csv.py
+++ utils/work_with_csv.py
@@ -1,3 +1,4 @@
+import itertools
 import os
 import pandas as pd
 
@@ -14,6 +15,26 @@ class CsvWriter:
         joined = ";".join(str(el) for el in iterable)
         self._file.write(f"{joined}\n")
 
+    def _append_complex_dict_to_csv(self, target_dict: dict):
+        complex_fields_keys = []
+        for key, value in target_dict.items():
+            if isinstance(value, list):
+                complex_fields_keys.append(key)
+
+        if complex_fields_keys:
+            itertools_args = [target_dict[key] for key in complex_fields_keys]
+
+            for fields in itertools.product(*itertools_args):
+                field_dict = dict(zip(complex_fields_keys, fields))
+
+                dict_for_record = target_dict.copy()
+
+                for key, value in field_dict.items():
+                    dict_for_record[key] = value
+
+                self._append_iterable_to_csv(dict_for_record.values())
+        else:
+            self._append_iterable_to_csv(target_dict.values())
 
     @property
     def first_recording(self) -> bool:
@@ -28,7 +49,7 @@ class CsvWriter:
             case dict(some_dict):
                 if self.first_recording:
                     self._append_iterable_to_csv(some_dict.keys())
-                self._append_iterable_to_csv(some_dict.values())
+                self._append_complex_dict_to_csv(some_dict)
             case list(some_iterable) | tuple(some_iterable):
                 self._append_iterable_to_csv(some_iterable)
             case _:
