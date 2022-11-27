b0e2f1e1729950cd86157f2713485071630aad34
Kitya <kittitereh@kittitereh.(none)>
2022-11-28 00:50:10 +0300

    fix recording of headers in csv

--- utils/work_with_csv.py
+++ utils/work_with_csv.py
@@ -5,6 +5,7 @@ import pandas as pd
 class CsvWriter:
     def __init__(self, csv_file_name: str, headers: list | tuple = None):
         self._file = open(csv_file_name, "w+")
+        self._first_recording = True
 
         if headers:
             self._append_iterable_to_csv(headers)
@@ -13,13 +14,19 @@ class CsvWriter:
         joined = ",".join(str(el) for el in iterable)
         self._file.write(f"{joined}\n")
 
-    def file_empty(self) -> bool:
-        return not self._file.read(1)
+
+    @property
+    def first_recording(self) -> bool:
+        if self._first_recording:
+            self._first_recording = False
+            return True
+        else:
+            return False
 
     def append_to_csv(self, smth_to_write: dict):
         match smth_to_write:
             case dict(some_dict):
-                if self.file_empty():
+                if self.first_recording:
                     self._append_iterable_to_csv(some_dict.keys())
                 self._append_iterable_to_csv(some_dict.values())
             case list(some_iterable) | tuple(some_iterable):
